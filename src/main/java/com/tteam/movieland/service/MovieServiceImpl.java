68f7af3b37177f2aa73daee0a6cce09677f7676f
Anton Obolonyk <36420446+Clio18@users.noreply.github.com>
2023-01-10 14:10:28 +0200

    Remove unnecessary imports

--- src/main/java/com/tteam/movieland/service/MovieServiceImpl.java
+++ src/main/java/com/tteam/movieland/service/MovieServiceImpl.java
@@ -1,6 +1,5 @@
 package com.tteam.movieland.service;
 
-import com.tteam.movieland.dto.MovieDto;
 import com.tteam.movieland.dto.mapper.EntityMapper;
 import com.tteam.movieland.entity.Movie;
 import com.tteam.movieland.exception.GenreNotFoundException;
