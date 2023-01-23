7fc704157ad9a02e8a53513d42bd0d907f786801
Oleksandr Shevchenko <4336399@gmail.com>
2023-01-23 13:00:01 +0200

    fixed application-dev.yml

--- src/main/java/com/tteam/movieland/MovielandApplication.java
+++ src/main/java/com/tteam/movieland/MovielandApplication.java
@@ -2,8 +2,9 @@ package com.tteam.movieland;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class MovielandApplication {
 
     public static void main(String[] args) {
