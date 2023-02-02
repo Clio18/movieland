6e274535ac26bde953ee363243e586601fb4c5c8
Oleksandr Shevchenko <4336399@gmail.com>
2023-02-02 12:36:43 +0200

    iteration 1 test fixes

---
+++ src/main/java/com/tteam/movieland/dto/CountryDto.java
@@ -0,0 +1,16 @@
+package com.tteam.movieland.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class CountryDto {
+    private Long id;
+
+    @JsonProperty("name")
+    private String countryName;
+}
