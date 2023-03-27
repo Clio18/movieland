b9f2c91dd56156e69d3b29e0f1fdb514c37566e7
Oleksandr Shevchenko <4336399@gmail.com>
2023-03-27 23:05:09 +0300

    reverting accidental changes

--- src/main/java/com/tteam/movieland/service/enrichment/ParallelEnrichMovieService.java
+++ src/main/java/com/tteam/movieland/service/enrichment/ParallelEnrichMovieService.java
@@ -1,5 +1,6 @@
 package com.tteam.movieland.service.enrichment;
 
+import com.tteam.movieland.dto.MovieDto;
 import com.tteam.movieland.entity.Country;
 import com.tteam.movieland.entity.Genre;
 import com.tteam.movieland.entity.Movie;
@@ -23,32 +24,76 @@ public class ParallelEnrichMovieService implements EnrichMovieService {
     private final ExecutorService cachedPool;
 
     @Override
-    public void enrich(Movie movie) {
+    public void enrich(MovieDto movieDto, Movie updatedMovie) {
+        Callable<Object> taskCountry = () -> {
+            log.info("Countries enrichment started...");
+            Set<Long> countriesIds = movieDto.getCountriesId();
+            //loadFunc();
+            Set<Country> countries = countryService.findAllById(countriesIds);
+            boolean isStop = Thread.currentThread().isInterrupted();
+            if (isStop) {
+                log.info("Parallel enrichment failed: movie has not been enriched with countries!");
+                return movieDto;
+            } else {
+                updatedMovie.setCountries(countries);
+                log.info("Parallel enrichment done: movie enriched with countries!");
+                return updatedMovie;
+            }
+        };
+
+        Callable<Object> taskGenre = () -> {
+            log.info("Genres enrichment started...");
+            Set<Long> genresIds = movieDto.getGenresId();
+            Set<Genre> genres = genreService.findAllById(genresIds);
+            boolean isStop = Thread.currentThread().isInterrupted();
+            if (isStop) {
+                log.info("Parallel enrichment failed: movie has not been enriched with genres!");
+                return movieDto;
+            } else {
+                updatedMovie.setGenres(genres);
+                log.info("Parallel enrichment done: movie enriched with genres!");
+                return updatedMovie;
+            }
+        };
+
+        try {
+            log.info("Parallel enrichment has been started...");
+            cachedPool.invokeAll(Set.of(taskCountry, taskGenre), 2, TimeUnit.SECONDS);
+            log.info("Parallel enrichment should be finished");
+            //loadFunc();
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            throw new RuntimeException("Exception from ThreadPool", e);
+        }
+    }
+
+    @Override
+    public void enrich(Movie updatedMovie) {
         Callable<Object> taskCountry = () -> {
             log.info("Countries enrichment started...");
             boolean isStop = Thread.currentThread().isInterrupted();
-            Set<Country> countries = countryService.findAllByMovieId(movie.getId());
+            Set<Country> countries = countryService.findAllByMovieId(updatedMovie.getId());
             if (isStop) {
                 log.info("Parallel enrichment failed: movie has not been enriched with countries!");
                 return new Object();
             } else {
-                movie.setCountries(countries);
+                updatedMovie.setCountries(countries);
                 log.info("Parallel enrichment done: movie enriched with countries!");
-                return movie;
+                return updatedMovie;
             }
         };
 
         Callable<Object> taskGenre = () -> {
             log.info("Genres enrichment started...");
             boolean isStop = Thread.currentThread().isInterrupted();
-            Set<Genre> genres = genreService.findAllByMovieId(movie.getId());
+            Set<Genre> genres = genreService.findAllByMovieId(updatedMovie.getId());
             if (isStop) {
                 log.info("Parallel enrichment failed: movie has not been enriched with genres!");
                 return new Object();
             } else {
-                movie.setGenres(genres);
+                updatedMovie.setGenres(genres);
                 log.info("Parallel enrichment done: movie enriched with genres!");
-                return movie;
+                return updatedMovie;
             }
         };
 
