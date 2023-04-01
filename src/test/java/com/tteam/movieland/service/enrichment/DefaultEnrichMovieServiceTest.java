dd9fd3c3fd6919d5b0ae889879b03d2cd434137e
U0L9511 <estclio09@gmail.com>
2023-04-01 13:52:53 +0300

    itr_2_v_13: Add some fixes to the tests

--- src/test/java/com/tteam/movieland/service/enrichment/DefaultEnrichMovieServiceTest.java
+++ src/test/java/com/tteam/movieland/service/enrichment/DefaultEnrichMovieServiceTest.java
@@ -4,11 +4,9 @@ import com.tteam.movieland.dto.mapper.MovieMapper;
 import com.tteam.movieland.service.CountryService;
 import com.tteam.movieland.service.GenreService;
 
-import java.util.concurrent.ExecutorService;
-
 public class DefaultEnrichMovieServiceTest extends EnrichMovieTest {
     @Override
-    protected EnrichMovieService getEnrichMovieService(CountryService countryService, GenreService genreService, ExecutorService executorService, MovieMapper mapper) {
+    protected EnrichMovieService getEnrichMovieService(CountryService countryService, GenreService genreService, MovieMapper mapper) {
         return new DefaultEnrichMovieService(countryService, genreService, mapper);
     }
 }
